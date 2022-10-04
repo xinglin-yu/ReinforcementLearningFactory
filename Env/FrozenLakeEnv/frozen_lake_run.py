a5c3a0472c4b7b4fc0cea01a75b215f31be45e46
xinglin-yu <xiaolinge360@gmail.com>
2022-10-04 22:48:19 +0800

    Frozen Lake fix

--- Env/FrozenLakeEnv/frozen_lake_run.py
+++ Env/FrozenLakeEnv/frozen_lake_run.py
@@ -1,6 +1,8 @@
+from CommonUtils.decorator import time_wrapper
 from Env.FrozenLakeEnv.frozen_lake_client import FrozenLakeClient
 from AgentAlgorithm.policy_iteration_client import PolicyIterationClient
 from AgentAlgorithm.value_iteration_client import ValueIterationClient
+from AgentAlgorithm.general_iteration_client import GeneralIterationClient
 
 
 class FrozenLakeRun:
@@ -20,8 +22,9 @@ class FrozenLakeRun:
         print(FrozenLakeClient.run_episodes(env, policy, episodes=10000, render=False))
 
     @staticmethod
+    @time_wrapper
     def policy_iteration_run():
-        env = FrozenLakeClient.create_preload_env_obj(order=4)
+        env = FrozenLakeClient.create_preload_env_obj(order=8)
 
         # get policy
         policy_client = PolicyIterationClient(env)
@@ -31,8 +34,9 @@ class FrozenLakeRun:
         FrozenLakeClient.show(env, policy, file_suffix="_PolicyIteration")
 
     @staticmethod
+    @time_wrapper
     def value_iteration_run():
-        env = FrozenLakeClient.create_preload_env_obj(order=4)
+        env = FrozenLakeClient.create_preload_env_obj(order=8)
 
         # get policy
         value_client = ValueIterationClient(env)
@@ -41,7 +45,20 @@ class FrozenLakeRun:
         # policy analysis and show
         FrozenLakeClient.show(env, policy, file_suffix="_ValueIteration")
 
+    @staticmethod
+    @time_wrapper
+    def general_iteration_run():
+        env = FrozenLakeClient.create_preload_env_obj(order=8)
+
+        # get policy
+        algo_client = GeneralIterationClient(env)
+        policy = algo_client.general_iteration(value_max_iter=500)
+
+        # policy analysis and show
+        FrozenLakeClient.show(env, policy, file_suffix="_GeneralIteration")
+
 
 if __name__ == '__main__':
-    # FrozenLakeRun.policy_iteration_run()
+    FrozenLakeRun.policy_iteration_run()
     FrozenLakeRun.value_iteration_run()
+    FrozenLakeRun.general_iteration_run()
